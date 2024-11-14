9b17c94c61bd05def30fa809eaed238bdb94bffd
Noman107c <se22f-107@ssuet.edu.pk>
2024-11-14 22:09:51 +0500

    lab4t1.java

---
+++ lab4t1.java
@@ -0,0 +1,6 @@
+class Main extends Thread{ public void run(){
+System.out.println("task one");
+public static void main(String args[]){
+Main tl=new Main();
+Maia t2=new Main(); Main t3=new Main(); t 1. start(); t 2. start(); t 3 . start();
+
